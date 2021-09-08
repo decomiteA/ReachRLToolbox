870682d08024013c73774f5f81db4fc464e50018
Morgane Moss <morganemoss@morganes-mbp-2.home>
2021-09-08 07:51:38 +0100

    testing new archs

--- forcefield/model.py
+++ forcefield/model.py
@@ -11,7 +11,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=16, fc2_units=8):
+    def __init__(self, state_size, action_size, seed, fc1_units=8):
         """Initialize parameters and build model.
         Params
         ======
@@ -24,26 +24,23 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.fc2 = nn.Linear(fc1_units, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
         x = torch.tanh(self.fc1(state))
-        x = torch.tanh(self.fc2(x))
-        return torch.tanh(self.fc3(x))
+        return torch.tanh(self.fc2(x))
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=16, fc2_units=8):
+    def __init__(self, state_size, action_size, seed, fc1_units=8, fc2_units=8):
         """Initialize parameters and build model.
         Params
         ======
