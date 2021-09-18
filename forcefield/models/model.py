9829e8f77ecfc39ae3ad0a8ee79a1abde8ee4a75
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2021-09-18 10:25:03 +0200

    remove seed

--- forcefield/models/model.py
+++ forcefield/models/model.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 def hidden_init(layer):
     fan_in = layer.weight.data.size()[0]
@@ -11,7 +13,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=16, fc2_units=8):
+    def __init__(self, state_size, action_size, fc1_units=16, fc2_units=8,alpha):
         """Initialize parameters and build model.
         Params
         ======
@@ -20,30 +22,49 @@ class Actor(nn.Module):
             seed (int): Random seed
             fc1_units (int): Number of nodes in first hidden layer
             fc2_units (int): Number of nodes in second hidden layer
+            alpha (float) : learning rate 
         """
         super(Actor, self).__init__()
-        self.seed = torch.manual_seed(seed)
+        
         self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
-        self.reset_parameters()
+        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data,-f1,f1)
+        torch.nn.init.uniform_(self.fc1.bias.data,-f1,f1)
+        self.bn1 = nn.LayerNorm(fc1_units)
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data,-f2,f2)
+        torch.nn.init.uniform_(self.fc2.bias.data,-f2,f2)
+        self.bn2 = nn.LayerNorm(fc2_units)
+
+        f3 = 0.003
+        self.mu = nn.Linear(fc2_units,action_size)
+        torch.nn.init.uniform_(self.mu.weight.data,-f3,f3)
+        torch.nn.init.uniform_(self.mu.bias.data,-f3,f3)
+        
+        self.optimizer = optim.Adam(self.parameters(),lr=alpha)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = torch.tanh(self.fc1(state))
-        x = torch.tanh(self.fc2(x))
-        return torch.tanh(self.fc3(x))
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = torch.tanh(self.mu(x))
+        return x
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=16, fc2_units=8):
+    def __init__(self, state_size, action_size, fc1_units=16, fc2_units=8, beta):
         """Initialize parameters and build model.
         Params
         ======
@@ -52,23 +73,47 @@ class Critic(nn.Module):
             seed (int): Random seed
             fc1_units (int): Number of nodes in the first hidden layer
             fc2_units (int): Number of nodes in the second hidden layer
+            beta (float) : learning rate for the critic
         """
         super(Critic, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units+action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
-        self.reset_parameters()
+        
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data,-f1,f1)
+        torch.nn.init.uniform_(self.fc1.bias.data,-f1,f1)
+        self.bn1 = nn.LayerNorm(fc1_units)
+
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data,-f2,f2)
+        torch.nn.init.uniform_(self.fc2.bias.data,-f2,f2)
+        self.bn2 = nn.LayerNorm(fc2_units)
+
+        self.action_value = nn.Linear(action_size,fc2_units)
+        f3=0.003
+        self.q = nn.Linear(fc2_units,1)
+        torch.nn.init.uniform_(self.q.weight.data,-f3,f3)
+        torch.nn.init.uniform_(self.q.bias.data,-f3,f3)
+        
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = torch.tanh(self.fc1(state))
-        x = torch.cat((xs, action), dim=1)
-        x = torch.tanh(self.fc2(x))
-        return self.fc3(x)
+        state_value = self.fc1(state)
+        state_value = self.bn1(state_value)
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state_value)
+        state_value = self.bn2(state_value)
+
+        action_value = F.relu(self.action_value(action))
+        state_action_value = F.relu(torch.add(state_value,action_value))
+        state_action_value = self.q(state_action_value)
+        return state_action_value
 
